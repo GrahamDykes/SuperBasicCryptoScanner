325898987cf718cf596204378ecc564636f39e97
GrahamDykes <153469002+GrahamDykes@users.noreply.github.com>
2024-05-26 10:44:22 -0700

    sunday start

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+const mongoose = require("mongoose");
 const app = express();
 
 const cors = require("cors");
@@ -14,13 +14,27 @@ app.use(express.json());
 app.use(require("./routes/record"));
 
 // Get MongoDB driver connection
-const dbo = require("./db/conn");
+// const dbo = require("./db/conn");
  
-app.listen(port, () => {
-  // Perform a database connection when server starts
-  dbo.connectToServer(function (err) {
-    if (err) console.error(err);
+// app.listen(port, () => {
+//   // Perform a database connection when server starts
+//   dbo.connectToServer(function (err) {
+//     if (err) console.error(err);
  
+//   });
+//   console.log(`Server is running on port: ${port}`);
+// });
+
+mongoose
+  .connect(process.env.ATLAS_URI)
+  .then(() => {
+    console.log("\n");
+    console.log("\n");
+    console.log("--Database Connected--");
+    app.listen(process.env.PORT, () => {
+      console.log(`App is listening to the ruckus on port ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("MongoDB error:\n", error);
   });
-  console.log(`Server is running on port: ${port}`);
-});
