3ee56c8dda27bf750f48494bd8491035abcfe702
GrahamDykes <153469002+GrahamDykes@users.noreply.github.com>
2024-06-08 20:47:33 -0700

    mostly dots, but i did learn about making a dictionary

--- frontend/src/components/justforgithub.js
+++ frontend/src/components/justforgithub.js
@@ -3,33 +3,30 @@ import "bootstrap/dist/css/bootstrap.css";
  
 
 export default function Git() {
+    console.log('\x1b[36m%s\x1b[0m', 'I am cyan on clear');  //cyan
+    console.log('\x1b[31m','\x1b[40m', 'I am Red on Black');  //red on black
 
 
-let testObj = {
-    'head': 'big',
-    'teeth': 25,
-    'chin' : 'square',
-    eyes:'two',
-    hair: 'blonde',
-    6: 'car',
-    7: 98,
+
+   // an array of items to count
+var arrayOfItemsToCount = ['a', 'b', 'a', 'b', 'c', 'b'];
+// an object to store the counts
+var objectOfCounts = {};
+
+// we loop over the array of items to count
+for (var i = 0; i < arrayOfItemsToCount.length; i++) {
+  var currentItem = arrayOfItemsToCount[i];
+  // if we have not counted current item
+  if (objectOfCounts[currentItem] === undefined) {
+    // add it to count object with a value of 1
+    objectOfCounts[currentItem] = 1;
+  // otherwise (we have counted it at least once)
+  } else {
+    // increment value in count object by 1
+    objectOfCounts[currentItem]++;
+  }
 }
-for(let x in testObj){
-    console.log('X: ',x)
-    console.log('testObj[x]', testObj[x])
-    
-    console.log('typeof:', typeof x)
-    console.log('-------\n')
-}
-
-
-
-
-
-
-
-
-
+console.log(objectOfCounts);
 
  return (
    <div>
--- frontend/src/components/variableTesting.js
+++ frontend/src/components/variableTesting.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.css";
-import { NavLink } from "react-router-dom";
+
  
 export default function variableTesting() {
  
@@ -40,67 +40,47 @@ const arrObjs = [
   },
 ];
 
-
-
-// for (let i = 0; i < arrObjs.length; i++){
-
-// }
-
 let testAns={}
 
 for (let x in arrObjs){
   console.log(`\n\n\n\tNew Iteration\n`)
     console.log(arrObjs[x])
-    // let surgery = Object.keys(arrObjs[x])
-    let surgery = Object.values(arrObjs[x])
-    console.log( `surgery: ${surgery}` )
-    console.log(`surgery[0]: ${surgery[0]}`)
-    console.log(`\ntestAns[surgery[0]] : ${testAns[surgery[0]]}\n`)
-    if(testAns[surgery[0]] === undefined){
-      // testAns[surgery[0]] = (testAns[surgery[3]]? [testAns[surgery[1]].concat('($)')]:testAns[surgery[1]])
-      testAns[surgery[0]] = 2
-    } else {
-      // testAns[surgery[0]] = [...testAns[surgery[0]],(testAns[surgery[3]]? [testAns[surgery[1]].concat('($)')]:testAns[surgery[1]])]
+  
+    console.log( `category: ${arrObjs[x].category}` )
+    console.log( `itemName: ${arrObjs[x].itemName}` )
+    console.log(`onSale:  ${arrObjs[x].onSale}`)
+    
+    if(testAns[arrObjs[x].category]===undefined){
+      if(arrObjs[x].onSale ==='true'){
+        testAns[arrObjs[x].category] = [arrObjs[x].itemName +"($)"]
+      }else{
+        testAns[arrObjs[x].category] = [arrObjs[x].itemName]
+      }
+    }else{
+      if(arrObjs[x].onSale ==='true'){
+        testAns[arrObjs[x].category].push([arrObjs[x].itemName +"($)"])
+      }else{
+        testAns[arrObjs[x].category].push([arrObjs[x].itemName])
+      }
     }
-console.log(`\ntestAns:\n${testAns[surgery[0]]}`)
+    console.log('\n')
+    console.log(`Current Answer Object: \n`, testAns)
+  }
+    
 
-}
 
-console.log(`\ntestAns:\n${testAns}`)
+console.log('\n')
+console.log('\n')
+console.log('\n')
+console.log(`\n Final Object :\n`)
 for(let x in testAns){
   console.log(`${x} : ${testAns[x]}`)
+  // console.log('Array.isArray:', Array.isArray(testAns[x]))
 }
 
-
-let answer = arrObjs.reduce((a, b)=>{
-  if ( !a[b.category] ){
-   a[b.category] = []
-  }
-  if( b.onSale == 'true'){
-      a[b.category].push(b.itemName.concat('($)'))
-  }else{
-   a[b.category].push(b.itemName)
-  }
-  return a
-},{});
-
-console.log(`\n\n\n\n`)
-for(let x in answer){
-  console.log(`${x} : ${answer[x]}`)
-}
-console.log(`\n\n\n\n`)
-console.log(`Answer:\n${answer}`)
-// let arr1 = []
-// let arr2 = []
-// let arr3 = []
-
-// let ansObj = {
-// property1:arr1,
-// property2:arr2,
-// property3:arr3
-// }
-
-// return ansObj
+console.log(`-------------------------`)
+console.log(`----END OF CYCLE-----`)
+console.log(`-------------------------`)
 
 
 return (
