bb37779837d1aa4e030aa3011cafb844a8d9d8ab
GrahamDykes <153469002+GrahamDykes@users.noreply.github.com>
2024-06-11 09:59:28 -0700

    dots dots dots everybody

--- frontend/src/components/justforgithub.js
+++ frontend/src/components/justforgithub.js
@@ -3,89 +3,48 @@ import "bootstrap/dist/css/bootstrap.css";
 import chalk from "chalk";
 
 export default function Git() {
-  // console.log('\x1b[36m%s\x1b[0m', 'I am cyan on clear');  //cyan
-  // console.log('\x1b[31m','\x1b[40m', 'I am Red on Black');  //red on black
+ 
+    var currentInventory = [
+        {
+          name: 'Brunello Cucinelli',
+          shoes: [
+            {name: 'tasselled black low-top lace-up', price: 1000},
+            {name: 'tasselled green low-top lace-up', price: 1100},
+            {name: 'plain beige suede moccasin', price: 950},
+            {name: 'plain olive suede moccasin', price: 1050}
+          ]
+        },
+        {
+          name: 'Gucci',
+          shoes: [
+            {name: 'red leather laced sneakers', price: 800},
+            {name: 'black leather laced sneakers', price: 900}
+          ]
+        }
+      ];
 
-  // FUNCTION DEFINITION(S)
-  function findMaxRepeatCountInWord(word) {
-    let wordArr = word.toLowerCase().split("");
-    // console.log("wordArr: ", wordArr);
-
-    let wordObj = wordArr.reduce((a, b) => {
-        
-    //   console.log("A:\n", a);
-    //   console.log("B:\n", b);
-
-      if (a[b] === undefined) {
-       a[b] = 1;
-       return a
-      } else {
-     a[b]++;
-     return a
-      }
-    //   console.log("A:\n", a);
-    }, {});
-    // console.log(`---------`);
-    // console.log("wordObj: ", wordObj);
-    let wordVals = Object.values(wordObj)
-    // console.log("wordVals: ", wordVals);
-    let winner = Math.max(...wordVals)
-    // console.log(chalk.blue.bold(`\nWinner of ${word}: \n`, winner));
-    // console.log("-------------------")
-    return winner
-  }
-
-//   findMaxRepeatCountInWord("cow");
-//   findMaxRepeatCountInWord("boymeetsword");
-//   findMaxRepeatCountInWord("sassafrass");
-//   findMaxRepeatCountInWord("cowabunga");
-
-  function findFirstWordWithMostRepeatedChars(text) {
-    var maxRepeatCountOverall = 0;
-    var wordWithMaxRepeatCount = "";
-
-    let wordsArr = text.toLowerCase().split(" ")
-    console.log("wordsArr: ", wordsArr);
-    let countArr= wordsArr.map(e=>findMaxRepeatCountInWord(e))
-    console.log("countArr: ", countArr);
-    console.log('Most repeated: ', Math.max(...countArr))
-    console.log(`index of winner?: `, countArr.indexOf(Math.max(...countArr)))
-    // Break up input text into words (space-delimited).
-    // For each word...
-    //   var repeatCountForWord = findMaxRepeatCountInWord(word)
-    // If that max repeat count is higher than the overall max repeat count, then
-    // update maxRepeatCountOverall
-    // update wordWithMaxRepeatCount
-
-    // return wordWithMaxRepeatCount;
-    console.log(`Winning word: `, wordsArr[countArr.indexOf(Math.max(...countArr))])
-    return wordsArr[countArr.indexOf(Math.max(...countArr))]
-  }
-
-  // ASSERTION FUNCTION(S) TO BE USED
-
-  // TESTS CASES
-
-  
-  findFirstWordWithMostRepeatedChars("i was walking down the street when horror struck")
-  findFirstWordWithMostRepeatedChars("i worship at the altar of slaughter")
-  findFirstWordWithMostRepeatedChars("BoneDaddy Mc Crunky time")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    function renderInventory(inventory) {
+        //we going to loop through the MAIN array
+        //grabbing each item from each designer
+        //throwing it into a master string
+        let masterString= ""
+        for(let obj in inventory){
+            for(let i = 0; i<inventory[obj].shoes.length;i++){
+                // console.log(`${inventory[obj].name}, ${inventory[obj].shoes[i].name}, ${inventory[obj].shoes[i].price}`)
+                console.log(`${inventory[obj].shoes[i].name.split(" ").includes('black')}`)
+                masterString+=`${inventory[obj].name}, ${inventory[obj].shoes[i].name}, ${inventory[obj].shoes[i].price}\n`
+            }
+        }
+        // for(let obj in inventory){
+        //     for(let i = 0; i<inventory[obj].shoes.length;i++){
+        //         console.log(`${inventory[obj].name}, ${inventory[obj].shoes[i].name}, ${inventory[obj].shoes[i].price}`)
+        //         masterString+=`${inventory[obj].name}, ${inventory[obj].shoes[i].name}, ${inventory[obj].shoes[i].price}\n`
+        //     }
+        // }
+        return masterString
+        }
+        renderInventory(currentInventory)
+        //Create helper functions if needed
 
 
 
