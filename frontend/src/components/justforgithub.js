69aa370e8000b31c2d5b8e64fb7ea0570682654a
GrahamDykes <153469002+GrahamDykes@users.noreply.github.com>
2024-07-09 16:16:32 -0700

    breezy

--- frontend/src/components/justforgithub.js
+++ frontend/src/components/justforgithub.js
@@ -1,47 +1,47 @@
-import React from "react";
-import "bootstrap/dist/css/bootstrap.css";
-import chalk from "chalk";
+// import React from "react";
+// import "bootstrap/dist/css/bootstrap.css";
+// import chalk from "chalk";
 
-export default function Git() {
+// export default function Git() {
  
-    var currentInventory = [
-        {
-          name: 'Brunello Cucinelli',
-          shoes: [
-            {name: 'tasselled black low-top lace-up', price: 1000},
-            {name: 'tasselled green low-top lace-up', price: 1100},
-            {name: 'plain beige suede moccasin', price: 950},
-            {name: 'plain olive suede moccasin', price: 1050}
-          ]
-        },
-        {
-          name: 'Gucci',
-          shoes: [
-            {name: 'red leather laced sneakers', price: 800},
-            {name: 'black leather laced sneakers', price: 900}
-          ]
-        }
-      ];
+//     var currentInventory = [
+//         {
+//           name: 'Brunello Cucinelli',
+//           shoes: [
+//             {name: 'tasselled black low-top lace-up', price: 1000},
+//             {name: 'tasselled green low-top lace-up', price: 1100},
+//             {name: 'plain beige suede moccasin', price: 950},
+//             {name: 'plain olive suede moccasin', price: 1050}
+//           ]
+//         },
+//         {
+//           name: 'Gucci',
+//           shoes: [
+//             {name: 'red leather laced sneakers', price: 800},
+//             {name: 'black leather laced sneakers', price: 900}
+//           ]
+//         }
+//       ];
 
-      function listAllBlackShoes(inventory) {
-        let masterString = ""
+//       function listAllBlackShoes(inventory) {
+//         let masterString = ""
         
-        for(let i = 0; i<inventory.length;i++){
-            for(let j = 0; j<inventory[i].shoes.length;j++){
-                if(inventory[i].shoes[j].name.split(" ").includes('black')){
-                    masterString+=`${inventory[i].name}, ${inventory[i].shoes[j].name}, ${inventory[i].shoes[j].price}\n`
-                }
-            }
-        }
+//         for(let i = 0; i<inventory.length;i++){
+//             for(let j = 0; j<inventory[i].shoes.length;j++){
+//                 if(inventory[i].shoes[j].name.split(" ").includes('black')){
+//                     masterString+=`${inventory[i].name}, ${inventory[i].shoes[j].name}, ${inventory[i].shoes[j].price}\n`
+//                 }
+//             }
+//         }
         
-        console.log(masterString)
-        return masterString
+//         console.log(masterString)
+//         return masterString
         
-        }
-        listAllBlackShoes(currentInventory)
-        //Create helper functions if needed
-        //get job! good show
-        //derping around
+//         }
+//         listAllBlackShoes(currentInventory)
+//         //Create helper functions if needed
+//         //get job! good show
+//         //derping around
 
 
 
@@ -51,9 +51,12 @@ export default function Git() {
 
 
 
-  return (
-    <div>
-      <p>Look at console</p>
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       <p>Look at console</p>
+//     </div>
+//   );
+// }
+
+
+//truly meaningless line
